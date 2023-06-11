96375bf2c24d6a92e5d980dd19b65beef0fc59a4
Prafull Patil <prafulpatil766254@gmail.com>
2023-06-11 20:04:19 +0530

    This is my mail sender branch

---
+++ src/main/java/org/example/MailAuthentication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class MailAuthentication extends Authenticator {
+    protected PasswordAuthentication getPasswordAuthentication()
+    {
+        return new PasswordAuthentication(MailDeta.SenderMail,MailDeta.appPassword);
+    }
+
+}
---
+++ src/main/java/org/example/MailDeta.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class MailDeta {
+
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String portNo = "465";
+    public static final String sslProperty = "mail.smtp.ssl.enable";
+    public static final String authPerm = "mail.smtp.auth";
+
+    public static final String SenderMail = "prafulpatil766254@gmail.com";
+
+    public static final String appPassword = "joupzcznsapxcinh";// Here we have used temporary app password from google to authenticate
+
+    public static final String receiverMail = "pp8729711@gmail.com";
+}
